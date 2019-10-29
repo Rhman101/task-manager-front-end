de83d7a7b90069d288a572cbd1a10a582748b2f2
Rhman101 <rhuysen@gmail.com>
2019-10-29 14:06:37 +0200

    Added CORS support

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import request from 'request';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // constructor(props) { // Useless Constructor
+  //   super(props);
+  // }
+
+  submitForm(e) {
+    e.preventDefault();
+    console.log('form submitted.')
+    request.post({
+      url: 'localhost:3000/users/login',
+      form: {
+        name: 'Test UserA',
+        email: 'rhuysen@gmail.com', 
+        age: 35,
+        password: 'asdjfhhwfweikh'
+      },
+      function(error, response, body) {
+        console.log('error', error);
+        console.log('response', response);
+        console.log('body', body);
+      }
+    })
+    // request.post('http://localhost:3000/users/login', (error, response, body) => {
+    //   console.log('error:', error);
+    //   console.log('statusCode:', response && response.statusCode)
+    //   console.log('body:', body)
+    // })
+  }
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.submitForm}>
+          <h1>Login Page</h1>
+          <p>Name and Surname</p>
+          <input type='text'></input>
+          <p>Age</p>
+          <input type='number'></input>
+          <p>Password</p>
+          <input type='password'></input>
+          <br></br>
+          <button type='submit'>Login</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
